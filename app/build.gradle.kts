cfd267ea26a012c55c90d08d205b0ce378d803c7
younjungpark <geoffrey.park@altibase.com>
2026-03-31 21:24:40 +0900

    버전코드를 5로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.bladderdiary.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 5
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
