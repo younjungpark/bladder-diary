46bcb0aed73e2eba72c20acdb7c2e87f834c73b5
younjungpark <geoffrey.park@altibase.com>
2026-05-09 11:57:35 +0900

    앱 버전을 1.0.4로 올림

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.chausoft.bladderdiary"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.3"
+        versionCode = 14
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
